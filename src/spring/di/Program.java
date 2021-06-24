3744e7cd1da0906d757b52904e9cb6a29a7dda02
jabberwocker0 <jabberwocker0@gmail.com>
2021-06-24 22:18:25 +0900

    콜렉션 생성과 목록 DI

--- src/spring/di/Program.java
+++ src/spring/di/Program.java
@@ -1,5 +1,8 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,6 +18,7 @@ public class Program {
 		
 		/* 스프링에게 지시하는 방법으로 코드를 변경
 		Exam exam = new NewLecExam();
+		Exam exam = new NewLecExam
 		ExamConsole console = new GridExamConsole(exam); // DI
 		
 		console.setExam(exam);
@@ -24,9 +28,18 @@ public class Program {
 		ApplicationContext context = 
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
 		
+		Exam exam = context.getBean(Exam.class);
+		System.out.println(exam.toString());
 		
-		//ExamConsole console = (ExamConsole) context.getBean("console"); // 오브젝트형을 받기때문에 형식변환 해줘야함(ExamConsole)로
-		ExamConsole console = context.getBean(ExamConsole.class); // 오브젝트형을 받기때문에 형식변환 해줘야함(ExamConsole)로
+		ExamConsole console = (ExamConsole) context.getBean("console"); // 오브젝트형을 받기때문에 형식변환 해줘야함(ExamConsole)로
+		//ExamConsole console = context.getBean(ExamConsole.class); // 오브젝트형을 받기때문에 형식변환 해줘야함(ExamConsole)로
 		console.print();
+		
+		List<Exam> exams = (List<Exam>) context.getBean("exams");//new ArrayList<>();
+		//exams.add(new NewLecExam(1,1,1,1));
+	
+		for(Exam e : exams) {
+			System.out.println(e);
+		}
 	}
 }
--- src/spring/di/entity/NewLecExam.java
+++ src/spring/di/entity/NewLecExam.java
@@ -7,6 +7,53 @@ public class NewLecExam implements Exam {
 	private int math;
 	private int com;
 	
+	public NewLecExam() {
+		
+	}
+	
+	
+	
+	public NewLecExam(int kor, int eng, int math, int com) {
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.com = com;
+	} // setting.xml에서 <constructor-arg에서 설정해준 것을 사용하는 값
+
+
+
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	}
+
+	public int getCom() {
+		return com;
+	}
+
+	public void setCom(int com) {
+		this.com = com;
+	}
+
 	@Override
 	public int total() {
 		return kor+eng+math+com;
@@ -16,4 +63,13 @@ public class NewLecExam implements Exam {
 		return total() / 4.0f;
 	}
 
+
+
+	@Override
+	public String toString() {
+		return "NewLecExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+	} // 국어 영어 등의 설정값을 리턴해준다
+
+	
+	
 }
